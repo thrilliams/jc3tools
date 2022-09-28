f9356365bf16f9ea87576d399fdadefa9d744f8d
Soren <soren@aethre.co>
2022-09-28 10:31:48 -0700

    implement png export!

--- src/converters/texture.ts
+++ src/converters/texture.ts
@@ -1,9 +1,12 @@
+import { encode } from 'https://deno.land/x/pngs@0.1.1/mod.ts';
+import decodeDxt from 'https://esm.sh/decode-dxt@1.0.1';
 import { TextureFile } from '../formats/TextureFile.ts';
 import { Buffer, readN, Uint32, writeFull } from '../deps.ts';
 import { Header } from '../squish/dds/Header.ts';
 import { HeaderFlags } from '../squish/dds/HeaderFlags.ts';
 import { PixelFormat } from '../squish/dds/PixelFormat.ts';
 import { FileFormat } from '../squish/dds/FileFormat.ts';
+import { PixelFormatFlags } from '../squish/dds/PixelFormatFlags.ts';
 
 async function exists(filename: string): Promise<boolean> {
 	try {
@@ -25,83 +28,85 @@ export function getPixelFormat(texture: TextureFile) {
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/bb173059.aspx "DXGI_FORMAT enumeration"
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/cc308051.aspx "Legacy Formats: Map Direct3D 9 Formats to Direct3D 10"
 
+	const pixelFormat = new PixelFormat();
+
 	switch (texture.format) {
-		case 71: {
-			// DXGI_FORMAT_BC1_UNORM
-			const pixelFormat = new PixelFormat();
+		case 71: // DXGI_FORMAT_BC1_UNORM
 			pixelFormat.intialize(FileFormat.DXT1);
 			return pixelFormat;
-		}
-
-		case 74: {
-			// DXGI_FORMAT_BC2_UNORM
-			const pixelFormat = new PixelFormat();
+		case 74: // DXGI_FORMAT_BC2_UNORM
 			pixelFormat.intialize(FileFormat.DXT3);
 			return pixelFormat;
-		}
-
-		case 77: {
-			// DXGI_FORMAT_BC3_UNORM
-			const pixelFormat = new PixelFormat();
+		case 77: // DXGI_FORMAT_BC3_UNORM
 			pixelFormat.intialize(FileFormat.DXT5);
 			return pixelFormat;
-		}
-		case 87: {
-			// DXGI_FORMAT_B8G8R8A8_UNORM
-			const pixelFormat = new PixelFormat();
+		case 87: // DXGI_FORMAT_B8G8R8A8_UNORM
 			pixelFormat.intialize(FileFormat.A8R8G8B8);
 			return pixelFormat;
-		}
-
 		case 61: // DXGI_FORMAT_R8_UNORM
 		case 80: // DXGI_FORMAT_BC4_UNORM
 		case 83: // DXGI_FORMAT_BC5_UNORM
-		case 98: {
-			// DXGI_FORMAT_BC7_UNORM
-			const pixelFormat = new PixelFormat();
+		case 98: // DXGI_FORMAT_BC7_UNORM
 			pixelFormat.size = pixelFormat.getSize();
 			pixelFormat.fourCC = 0x30315844; // 'DX10'
 			return pixelFormat;
-		}
 	}
 
 	throw new Error('NotSupportedException');
 }
 
-export async function saveDDSFile(
+export async function prepareDdscAndHeader(
 	outputBaseName: string,
 	elementIndex: number,
 	texture: TextureFile,
 	ddsc: Buffer | null
 ) {
 	const hmddscFile = outputBaseName + '.hmddsc';
-	const fileName = outputBaseName + elementIndex + '.dds';
 
 	if (ddsc === null) {
-		if (!(await exists(hmddscFile))) return false;
+		if (!(await exists(hmddscFile))) throw new Error('TextureNotFound');
 		const ddscFile = await Deno.readFile(hmddscFile);
 		ddsc = new Buffer(ddscFile);
 	}
 
 	let rank = 0;
 	for (let i = 0; i < texture.elements.length; i++) {
-		if (i == elementIndex) continue;
+		if (i === elementIndex) continue;
 		if (texture.elements[i].size > texture.elements[elementIndex].size) rank++;
 	}
 
 	// create the DDS header
 	const header = new Header({
-		Size: 124,
-		Flags: HeaderFlags.Texture | HeaderFlags.Mipmap,
-		Width: texture.width >> rank,
-		Height: texture.height >> rank,
-		PitchOrLinearSize: 0,
-		Depth: texture.depth,
-		MipMapCount: 1, // always 1
-		PixelFormat: getPixelFormat(texture),
-		SurfaceFlags: 8 | 0x1000,
-		CubemapFlags: 0
+		size: 124,
+		flags: HeaderFlags.Texture | HeaderFlags.Mipmap,
+		width: texture.width >> rank,
+		height: texture.height >> rank,
+		pitchOrLinearSize: 0,
+		depth: texture.depth,
+		mipMapCount: 1, // always 1
+		pixelFormat: getPixelFormat(texture),
+		surfaceFlags: 8 | 0x1000,
+		cubemapFlags: 0
 	});
+
+	return { header, ddsc };
+}
+
+export async function saveDdsFile(
+	outputBaseName: string,
+	elementIndex: number,
+	texture: TextureFile,
+	srcDdsc: Buffer | null
+) {
+	const fileName = outputBaseName + elementIndex + '.dds';
+
+	const ddscAndHeader = await prepareDdscAndHeader(
+		outputBaseName,
+		elementIndex,
+		texture,
+		srcDdsc
+	);
+	const { header, ddsc } = ddscAndHeader;
 
 	const output = new Buffer();
 
@@ -110,7 +115,7 @@ export async function saveDDSFile(
 	await header.serialize(output, 'le');
 
 	// DX10 header
-	if (header.PixelFormat.fourCC == 0x30315844) {
+	if (header.pixelFormat.fourCC == 0x30315844) {
 		await Uint32.le.writeTo(output, texture.format);
 		await Uint32.le.writeTo(output, 3); // was 2. should be 3 as we most likely will export 2D textures
 		await Uint32.le.writeTo(output, 0);
@@ -123,10 +128,95 @@ export async function saveDDSFile(
 	const elementBytes = await readN(ddsc, texture.elements[elementIndex].size);
 	await writeFull(output, elementBytes);
 
+	// write to disk
 	await Deno.writeFile(fileName, output.bytes());
 }
 
-export async function exportTextureFile(ddscTextureFile: string, outputBaseName: string) {
+export function ddsBodyToRgba(body: Uint8Array, header: Header) {
+	if (
+		header.pixelFormat.flags === PixelFormatFlags.RGB ||
+		header.pixelFormat.flags === PixelFormatFlags.RGBA
+	) {
+		// TODO: implement
+		throw new Error('not implemented');
+	} else {
+		let format = '';
+		switch (header.pixelFormat.fourCC) {
+			case 0x31545844:
+				format = 'dxt1';
+				break;
+			case 0x33545844:
+				format = 'dxt3';
+				break;
+			case 0x35545844:
+				format = 'dxt5';
+				break;
+		}
+		return decodeDxt(new DataView(body.buffer), header.width, header.height, format);
+	}
+}
+
+export async function savePngFile(
+	outputBaseName: string,
+	elementIndex: number,
+	texture: TextureFile,
+	srcDdsc: Buffer | null
+) {
+	const fileName = outputBaseName + elementIndex + '.png';
+
+	const ddscAndHeader = await prepareDdscAndHeader(
+		outputBaseName,
+		elementIndex,
+		texture,
+		srcDdsc
+	);
+	const { header, ddsc } = ddscAndHeader;
+
+	Object.assign(ddsc, { '#off': texture.elements[elementIndex].offset }); // this sucks (and works!)
+	const elementBytes = await readN(ddsc, texture.elements[elementIndex].size);
+
+	const rgba = await ddsBodyToRgba(elementBytes, header);
+
+	const image = encode(rgba, header.width, header.height);
+
+	// write to disk
+	await Deno.writeFile(fileName, image);
+}
+
+interface ExportTextureMetadata {
+	texture: {
+		writeToHmddsc: boolean;
+
+		header: {
+			unknown06: string;
+			unknown1C: string;
+			dimension: number;
+			depth: number;
+			mipCount: number;
+			hdrMipCount: number;
+			flags: string;
+		};
+
+		check: {
+			width: number;
+			height: number;
+			format: number;
+		};
+
+		elements: {
+			offset: number;
+			size: number;
+			external: boolean;
+			unknown8: string;
+			unknownA: string;
+		}[];
+	};
+}
+
+export async function exportTextureFile(
+	ddscTextureFile: string,
+	outputBaseName: string
+): Promise<ExportTextureMetadata> {
 	// const xmlOutFile = outputBaseName + '.xml';
 	const hmddscFile = outputBaseName + '.hmddsc';
 
@@ -151,50 +241,41 @@ export async function exportTextureFile(ddscTextureFile: string, outputBaseName:
 		}
 	}
 
+	// if (texture.elements[biggestIndex].isExternal == false)
+	// 	await saveDdsFile(outputBaseName, biggestIndex, texture, input); // load internal texture
+	// else await saveDdsFile(outputBaseName, biggestIndex, texture, null); // load external texture (from hmddsc file)
+
 	if (texture.elements[biggestIndex].isExternal == false)
-		await saveDDSFile(outputBaseName, biggestIndex, texture, input); // load internal texture
-	else await saveDDSFile(outputBaseName, biggestIndex, texture, null); // load external texture (from hmddsc file)
+		await savePngFile(outputBaseName, biggestIndex, texture, input); // load internal texture
+	else await savePngFile(outputBaseName, biggestIndex, texture, null); // load external texture (from hmddsc file)
 
-	// TODO:
-	// the XML metadata
-	// using (var xmlOutput = File.Create(xmlOutFile))
-	// using (var writer = XmlWriter.Create(xmlOutput, settings))
-	// {
-	// 	writer.WriteStartDocument();
-	// 	writer.WriteStartElement("texture");
-	// 	writer.WriteAttributeString("write-to-hmddsc", haveHMDDSCFile.ToString(CultureInfo.InvariantCulture));
+	return {
+		texture: {
+			writeToHmddsc: haveHMDDSCFile,
 
-	// 	writer.WriteStartElement("header");
-	// 	writer.WriteAttributeString("unknown-06", texture.Unknown06.ToString("X8"));
-	// 	writer.WriteAttributeString("unknown-1C", texture.Unknown1C.ToString("X8"));
-	// 	writer.WriteAttributeString("dimension", texture.Dimension.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteAttributeString("depth", texture.Depth.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteAttributeString("mip-count", texture.MipCount.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteAttributeString("hdr-mip-count", texture.HeaderMipCount.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteAttributeString("flags", texture.Flags.ToString("X8"));
-	// 	writer.WriteEndElement();
+			header: {
+				unknown06: texture.unknown06.toString(8),
+				unknown1C: texture.unknown1C.toString(8),
+				dimension: texture.dimension,
+				depth: texture.depth,
+				mipCount: texture.mipCount,
+				hdrMipCount: texture.headerMipCount,
+				flags: texture.flags.toString(8)
+			},
 
-	// 	// Will emit a warning (or sometime fail) if not the same
-	// 	writer.WriteStartElement("check");
-	// 	writer.WriteAttributeString("width", texture.Height.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteAttributeString("height", texture.Width.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteAttributeString("format", texture.Format.ToString(CultureInfo.InvariantCulture));
-	// 	writer.WriteEndElement();
+			check: {
+				width: texture.width,
+				height: texture.height,
+				format: texture.format
+			},
 
-	// 	writer.WriteStartElement("elements");
-	// 	foreach (var element in texture.Elements)
-	// 	{
-	// 		writer.WriteStartElement("element");
-	// 		writer.WriteAttributeString("offset", element.Offset.ToString(CultureInfo.InvariantCulture));
-	// 		writer.WriteAttributeString("size", element.Size.ToString(CultureInfo.InvariantCulture));
-	// 		writer.WriteAttributeString("external", element.IsExternal.ToString(CultureInfo.InvariantCulture));
-	// 		writer.WriteAttributeString("unknown8", element.Unknown8.ToString("X8"));
-	// 		writer.WriteAttributeString("unknownA", element.UnknownA.ToString("X8"));
-	// 		writer.WriteEndElement();
-	// 	}
-	// 	writer.WriteEndElement();
-
-	// 	writer.WriteEndElement();
-	// 	writer.WriteEndDocument();
-	// }
+			elements: texture.elements.map((element) => ({
+				offset: element.offset,
+				size: element.size,
+				external: element.isExternal,
+				unknown8: element.unknown8.toString(8),
+				unknownA: element.unknownA.toString(8)
+			}))
+		}
+	};
 }
--- src/squish/dds/Header.ts
+++ src/squish/dds/Header.ts
@@ -3,87 +3,87 @@ import { PixelFormat } from './PixelFormat.ts';
 import { HeaderFlags } from './HeaderFlags.ts';
 
 interface ConstructorOptions {
-	PixelFormat?: PixelFormat;
-	Flags?: HeaderFlags;
+	pixelFormat?: PixelFormat;
+	flags?: HeaderFlags;
 
-	Size?: number;
-	Height?: number;
-	Width?: number;
-	PitchOrLinearSize?: number;
-	Depth?: number;
-	MipMapCount?: number;
-	SurfaceFlags?: number;
-	CubemapFlags?: number;
+	size?: number;
+	height?: number;
+	width?: number;
+	pitchOrLinearSize?: number;
+	depth?: number;
+	mipMapCount?: number;
+	surfaceFlags?: number;
+	cubemapFlags?: number;
 }
 
 export class Header {
-	PixelFormat: PixelFormat;
-	Flags!: HeaderFlags;
+	pixelFormat: PixelFormat;
+	flags!: HeaderFlags;
 
-	Size!: number;
-	Height!: number;
-	Width!: number;
-	PitchOrLinearSize!: number;
-	Depth!: number;
-	MipMapCount!: number;
-	SurfaceFlags!: number;
-	CubemapFlags!: number;
+	size!: number;
+	height!: number;
+	width!: number;
+	pitchOrLinearSize!: number;
+	depth!: number;
+	mipMapCount!: number;
+	surfaceFlags!: number;
+	cubemapFlags!: number;
 
-	Reserved1 = new ArrayBuffer(11 * 4);
-	Reserved2 = new ArrayBuffer(3 * 4);
+	reserved1 = new ArrayBuffer(11 * 4);
+	reserved2 = new ArrayBuffer(3 * 4);
 
 	constructor(options?: ConstructorOptions) {
-		this.PixelFormat = new PixelFormat();
+		this.pixelFormat = new PixelFormat();
 
 		if (!options) return;
 
-		if (options.PixelFormat) this.PixelFormat = options.PixelFormat;
-		if (options.Flags) this.Flags = options.Flags;
+		if (options.pixelFormat) this.pixelFormat = options.pixelFormat;
+		if (options.flags) this.flags = options.flags;
 
-		if (options.Size) this.Size = options.Size;
-		if (options.Height) this.Height = options.Height;
-		if (options.Width) this.Width = options.Width;
-		if (options.PitchOrLinearSize) this.PitchOrLinearSize = options.PitchOrLinearSize;
-		if (options.Depth) this.Depth = options.Depth;
-		if (options.MipMapCount) this.MipMapCount = options.MipMapCount;
-		if (options.SurfaceFlags) this.SurfaceFlags = options.SurfaceFlags;
-		if (options.CubemapFlags) this.CubemapFlags = options.CubemapFlags;
+		if (options.size) this.size = options.size;
+		if (options.height) this.height = options.height;
+		if (options.width) this.width = options.width;
+		if (options.pitchOrLinearSize) this.pitchOrLinearSize = options.pitchOrLinearSize;
+		if (options.depth) this.depth = options.depth;
+		if (options.mipMapCount) this.mipMapCount = options.mipMapCount;
+		if (options.surfaceFlags) this.surfaceFlags = options.surfaceFlags;
+		if (options.cubemapFlags) this.cubemapFlags = options.cubemapFlags;
 	}
 
 	GetSize() {
-		return 18 * 4 + this.PixelFormat.getSize() + 5 * 4;
+		return 18 * 4 + this.pixelFormat.getSize() + 5 * 4;
 	}
 
 	async serialize(output: Buffer, endian: Endian) {
-		await Uint32[endian].writeTo(output, this.Size);
-		await Uint32[endian].writeTo(output, this.Flags);
-		await Int32[endian].writeTo(output, this.Height);
-		await Int32[endian].writeTo(output, this.Width);
-		await Uint32[endian].writeTo(output, this.PitchOrLinearSize);
-		await Uint32[endian].writeTo(output, this.Depth);
-		await Uint32[endian].writeTo(output, this.MipMapCount);
-		await writeFull(output, new Uint8Array(this.Reserved1));
-		await this.PixelFormat.serialize(output, endian);
-		await Uint32[endian].writeTo(output, this.SurfaceFlags);
-		await Uint32[endian].writeTo(output, this.CubemapFlags);
-		await writeFull(output, new Uint8Array(this.Reserved2));
+		await Uint32[endian].writeTo(output, this.size);
+		await Uint32[endian].writeTo(output, this.flags);
+		await Int32[endian].writeTo(output, this.height);
+		await Int32[endian].writeTo(output, this.width);
+		await Uint32[endian].writeTo(output, this.pitchOrLinearSize);
+		await Uint32[endian].writeTo(output, this.depth);
+		await Uint32[endian].writeTo(output, this.mipMapCount);
+		await writeFull(output, new Uint8Array(this.reserved1));
+		await this.pixelFormat.serialize(output, endian);
+		await Uint32[endian].writeTo(output, this.surfaceFlags);
+		await Uint32[endian].writeTo(output, this.cubemapFlags);
+		await writeFull(output, new Uint8Array(this.reserved2));
 	}
 
 	async deserialize(input: Buffer, endian: Endian) {
-		this.Size = await Uint32[endian].readFrom(input);
+		this.size = await Uint32[endian].readFrom(input);
 		// assuming this is a byte flag 🤞
-		this.Flags = await Uint32[endian].readFrom(input);
-		this.Height = await Int32[endian].readFrom(input);
-		this.Width = await Int32[endian].readFrom(input);
-		this.PitchOrLinearSize = await Uint32[endian].readFrom(input);
-		this.Depth = await Uint32[endian].readFrom(input);
-		this.MipMapCount = await Uint32[endian].readFrom(input);
+		this.flags = await Uint32[endian].readFrom(input);
+		this.height = await Int32[endian].readFrom(input);
+		this.width = await Int32[endian].readFrom(input);
+		this.pitchOrLinearSize = await Uint32[endian].readFrom(input);
+		this.depth = await Uint32[endian].readFrom(input);
+		this.mipMapCount = await Uint32[endian].readFrom(input);
 		// there's a chance this doesn't error when it should
-		this.Reserved1 = await readFull(input, this.Reserved1);
-		this.PixelFormat.deserialize(input, endian);
-		this.SurfaceFlags = await Uint32[endian].readFrom(input);
-		this.CubemapFlags = await Uint32[endian].readFrom(input);
+		this.reserved1 = await readFull(input, this.reserved1);
+		this.pixelFormat.deserialize(input, endian);
+		this.surfaceFlags = await Uint32[endian].readFrom(input);
+		this.cubemapFlags = await Uint32[endian].readFrom(input);
 		// there's a chance this doesn't error when it should
-		this.Reserved1 = await readFull(input, this.Reserved2);
+		this.reserved1 = await readFull(input, this.reserved2);
 	}
 }
